fd9dcfca6e1692cfad8c48d1d62d7ad3c7e218b6
Phangsham jugli <phangshamjugli@gmail.com>
2026-08-27 00:19:27 +0530

    list and its operations added

---
+++ List in Python/LP1.py
@@ -0,0 +1,21 @@
+#list in python
+student=["John",20,70.4]
+print(student)
+print(type(student))  #to know the datatype
+print("\n")
+
+#indexing list
+days_of_a_week=['monday','tuesday','wednesday','thursday','friday','saturday','sunday']
+print(days_of_a_week)
+print("\n")
+print(days_of_a_week[0])
+print(days_of_a_week[1])
+print(days_of_a_week[2])
+print(days_of_a_week[3])
+print(days_of_a_week[4])
+print(days_of_a_week[5])
+print(days_of_a_week[6])
+print(f"last day of week is {days_of_a_week[-1]}")
+print(len(days_of_a_week)) #checking length of the week
+
+
---
+++ List in Python/LP2.py
@@ -0,0 +1,31 @@
+#slicing of list
+l1=[1,24,6,8,233,0,90,22,45]
+print(l1)
+print(l1[1:7:1])
+print(l1[1:7:2])
+print("\n")
+
+#concatenation of lists
+l2=[2,3,4]
+l3=[5,6,7,7]
+print(l2+l3)
+print("\n")
+print(l1+l2+l3)
+print("\n")
+
+#repeat of list
+print(l2*3)
+print("\n")
+
+#function of list
+fruits=["apple","banana","orange"]
+print(fruits)
+print("\n")
+
+#1.append()
+fruits.append("mango")
+print(fruits)
+
+#2.insert()
+fruits.insert(1,"litchi")
+print(fruits)
