f14600ce4f5095fa440fac66e836284894d010e6
Phangsham jugli <phangshamjugli@gmail.com>
2026-08-27 21:45:32 +0530

    numeric function added

--- Python Basic/PB6.py
+++ Python Basic/PB6.py
@@ -8,3 +8,13 @@ print(age)
 print("your name is ",name,"and your age is ",age)
 
 #--------------Numeric Function-------------------
+num=1,2,3,4,5,6,7,8,9
+#min()
+m=min(num)
+print(m)
+#max()
+x=max(num)
+print(x)
+
+#sum()
+print(sum(num))
