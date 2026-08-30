9b76b47f7364dba13a101312d241ee906dc8e869
Phangsham jugli <phangshamjugli@gmail.com>
2026-08-30 21:41:35 +0530

    changes made in Practice folder

--- Practice/practice2.py
+++
@@ -1,6 +0,0 @@
-#Take name, age, and course as input from the user and print a sentence using an f-string, for example:
-name=input("Enter your name:")
-age=int(input("Enter your age:"))
-course=input("Enter you course:")
-print("\n")
-print(f"Hello!{name},your age is {age} and your currently studying {course}")
